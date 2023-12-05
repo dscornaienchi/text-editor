884b04011c0c9b66bd0159cb79d45b9b4fc5854c
dscornaienchi <Dscorni32@gmail.com>
2023-12-05 15:43:10 -0800

    application startup complete

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const htmlRoutes = require('./routes/htmlRoutes'); // Import the htmlRoutes module
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'client/dist' directory
+app.use(express.static(path.join(__dirname, './src',)));
+
+// Use the htmlRoutes module for handling HTML routes
+htmlRoutes(app);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
